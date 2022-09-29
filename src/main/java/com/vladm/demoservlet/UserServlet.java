e6322895ead818e4dcf6da8b5e341d7b64f47383
vladm <vlad.milytin@gmail.com>
2022-09-29 19:45:28 +0300

    Simple user create

--- src/main/java/com/vladm/demoservlet/UserServlet.java
+++ src/main/java/com/vladm/demoservlet/UserServlet.java
@@ -9,17 +9,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.UUID;
 
 @WebServlet(name = "userServlet", value = "/user")
 public class UserServlet extends HttpServlet {
 
-    public User user = new User("User", "email@test.com");
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        User user = createUser(name, email);
+
         request.setAttribute("user", user);
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("user.jsp");
 
         requestDispatcher.forward(request, response);
+    }
+
+    public User createUser(String name, String email) {
+        final String id = UUID.randomUUID().toString();
+        return new User(id, name, email);
     }
 }
--- src/main/java/com/vladm/demoservlet/model/User.java
+++ src/main/java/com/vladm/demoservlet/model/User.java
@@ -1,11 +1,15 @@
 package com.vladm.demoservlet.model;
 
+import java.util.Objects;
+
 public class User {
 
+    public String id;
     public String name;
     public String email;
 
-    public User(String name, String email){
+    public User(String id, String name, String email){
+        this.id = id;
         this.name = name;
         this.email = email;
     }
@@ -24,5 +28,35 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
